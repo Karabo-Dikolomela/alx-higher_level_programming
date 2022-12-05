ba8dea4b1086aad1328615f07740b79abc074670
Karabo-Dikolomela <108591320+Karabo-Dikolomela@users.noreply.github.com>
2022-12-06 00:00:17 +0200

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.4
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return (my_list)
@@ -10,4 +10,4 @@ def replace_in_list(my_list, idx, element):
 
     my_list[idx] = element
 
- return (my_list)
+    return (my_list)
